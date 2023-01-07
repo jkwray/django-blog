fd19f89c23242a08253c0341f7633182a2e53cd0
jkwray <38891054+jkwray@users.noreply.github.com>
2023-01-07 13:37:06 -0800

    Excluded posts from category admin page

--- mysite/blogging/admin.py
+++ mysite/blogging/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
+# class CategoryInline(admin.TabularInline):
+#     # model = Category
+#     pass
+
+# class PostAdmin(admin.ModelAdmin):
+#     inlines = [CategoryInline, ]
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+
 admin.site.register(Post)
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
