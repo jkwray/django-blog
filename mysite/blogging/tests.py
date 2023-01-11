db5739dfec30ee7a9d61795d23c1a6c5bf03c39e
jkwray <38891054+jkwray@users.noreply.github.com>
2023-01-11 14:06:31 -0800

    Applied black.

--- mysite/blogging/tests.py
+++ mysite/blogging/tests.py
@@ -4,18 +4,20 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from blogging.models import Post, Category
 
+
 class FrontEndTestCase(TestCase):
     """test views provided on the front end"""
-    fixtures = ['blogging_test_fixture.json', ]
+
+    fixtures = [
+        "blogging_test_fixture.json",
+    ]
 
     def setUp(self):
         self.now = datetime.datetime.utcnow().replace(tzinfo=utc)
         self.timedelta = datetime.timedelta(15)
         author = User.objects.get(pk=1)
         for count in range(1, 11):
-            post = Post(title="Post %d Title" % count,
-                text="foo",
-                author=author)
+            post = Post(title="Post %d Title" % count, text="foo", author=author)
             if count < 6:
                 # publish the first five posts
                 pubdate = self.now - self.timedelta * count
@@ -23,7 +25,7 @@ class FrontEndTestCase(TestCase):
             post.save()
 
     def test_list_only_published(self):
-        resp = self.client.get('/')
+        resp = self.client.get("/")
         resp_text = resp.content.decode(resp.charset)
         self.assertTrue("My Cool Blog Posts" in resp_text)
         for count in range(1, 11):
@@ -32,12 +34,12 @@ class FrontEndTestCase(TestCase):
                 self.assertContains(resp, title, count=1)
             else:
                 self.assertNotContains(resp, title)
-    
+
     def test_details_only_published(self):
         for count in range(1, 11):
             title = "Post %d Title" % count
             post = Post.objects.get(title=title)
-            resp = self.client.get('/posts/%d/' % post.pk)
+            resp = self.client.get("/posts/%d/" % post.pk)
             if count < 6:
                 self.assertEqual(resp.status_code, 200)
                 self.assertContains(resp, title)
@@ -46,7 +48,9 @@ class FrontEndTestCase(TestCase):
 
 
 class PostTestCase(TestCase):
-    fixtures = ['blogging_test_fixture.json', ]
+    fixtures = [
+        "blogging_test_fixture.json",
+    ]
 
     def setUp(self):
         self.user = User.objects.get(pk=1)
@@ -57,8 +61,8 @@ class PostTestCase(TestCase):
         actual = str(p1)
         self.assertEqual(expected, actual)
 
+
 class CategoryTestCase(TestCase):
-    
     def test_string_representation(self):
         expected = "A Category"
         c1 = Category(name=expected)
