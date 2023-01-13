6268bc70b0cb04e211670d9c7a3b51e62025a4d0
jkwray <38891054+jkwray@users.noreply.github.com>
2023-01-12 18:00:48 -0800

    Basic add blog functionality added

--- mysite/blogging/models.py
+++ mysite/blogging/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=128)
@@ -12,6 +12,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("/")
 
 
 class Category(models.Model):
--- mysite/blogging/views.py
+++ mysite/blogging/views.py
@@ -3,6 +3,7 @@ from django.template import loader
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic import CreateView
 from blogging.models import Post
 
 
@@ -18,3 +19,8 @@ class PostDetailView(DetailView):
     context_object_name = "post_detail"
     queryset = Post.objects.exclude(published_date__exact=None)
     template_name = "blogging/detail.html"
+
+class PostAddView(CreateView):
+    model = Post
+    template_name = "blogging/add.html"
+    fields = "__all__"
